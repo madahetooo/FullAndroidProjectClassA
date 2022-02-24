d0bcc46782339fbef0f539d4bc8d430b7ca4a026
madahetooo <ieslammedhat@gmail.com>
2022-02-24 17:51:55 +0200

    Adding runBlocking , Join, Cancel, Asyn, Await

--- app/src/main/java/com/apps/fullandroidcourseclassa/ui/CoroutineExamples.kt
+++ app/src/main/java/com/apps/fullandroidcourseclassa/ui/CoroutineExamples.kt
@@ -4,40 +4,49 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.apps.fullandroidcourseclassa.R
-import kotlinx.android.synthetic.main.activity_coroutine_examples.*
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class CoroutineExamples : AppCompatActivity() {
-    val TAG ="CoroutineExamples"
+    val TAG = "CoroutineExamples"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coroutine_examples)
 
-//        GlobalScope.launch {
-//            delay(5000L)
-//            Log.d(TAG,"Coroutine says hello from Thread : ${Thread.currentThread().name}")
-//        }
-//        Log.d(TAG, "hello from Thread : ${Thread.currentThread().name}")
-
-
-        //OPERATION THREAD (BACKGROUND TASKS)
-        GlobalScope.launch(Dispatchers.IO) {
-            Log.d(TAG,"Starting coroutine in thread ${Thread.currentThread().name} ") //IO THREAD
-
-           val updatedTextInDesign = doNetworkCall()
-
-            //CONTEXT SWITCH TO "MAIN" THREAD
-            withContext(Dispatchers.Main){
-                tvDummy.text = updatedTextInDesign
-                Log.d(TAG,"Setting coroutine in thread ${Thread.currentThread().name} ")
+        GlobalScope.async(Dispatchers.IO) {
+            val time = measureTimeMillis {
+                val answer1 = async { networkCall1() }
+                val answer2 = async { networkCall2() }
+                Log.d(TAG,"Answer 1 is ${answer1.await()}")
+                Log.d(TAG,"Answer 2 is ${answer2.await()}")
             }
-
+            Log.d(TAG,"Request took  $time  ms")
         }
+        runBlocking {
+            launch {
 
+            }
+        }
     }
 
-    suspend fun doNetworkCall():String{
-        delay(4000L)
-        return "This is the return String"
+    suspend fun networkCall1():String{
+        delay(3000L)
+        return "Answer 1"
+    }
+    suspend fun networkCall2():String{
+        delay(3000L)
+        return "Answer 2"
     }
 }
+//var answer1 :String? =null
+//            var answer2 :String? =null
+//            val job1 = launch {
+//                answer1 = networkCall1()
+//            }
+//            val job2 = launch {
+//                answer2 = networkCall2()
+//            }
+//            job1.join()
+//            job2.join()
+//            Log.d(TAG,"Answer 1 is $answer1")
+//            Log.d(TAG,"Answer 2 is $answer2")
